a0fd22cf5016f2509eddbfc49a41bbadcd8806b4
zsp <1451023577@qq.com>
2021-11-22 22:10:40 +0800

    设置utf-8编码

--- wechat_auth/handle.py
+++ wechat_auth/handle.py
@@ -16,6 +16,9 @@ class Handle(object):
             list = [token, timestamp, nonce]
             list.sort()
             sha1 = hashlib.sha1()
+            sha1.update(list[0].encode('utf-8'))
+            sha1.update(list[1].encode('utf-8'))
+            sha1.update(list[2].encode('utf-8'))
             map(sha1.update, list)
             hashcode = sha1.hexdigest()
             print ("handle/GET func: hashcode, signature: ", hashcode, signature)
